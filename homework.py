37b36532c5b25c4d1c6969b2c36651c498462fff
Anthony Esquivel <aesquivelacademics@gmail.com>
2024-04-23 11:36:01 -0700

    First Commit

---
+++ homework.py
@@ -0,0 +1,59 @@
+#Showcase your dance moves
+weather="sunny"
+if weather=="sunny":
+    print("Wear Sunglasses")
+else:
+     print("Take an Umbrella")
+#mood input 
+mood=input("How are you feeling today?: ")
+if mood == "happy":
+     print("Thats great to hear")
+elif mood == "sad":
+     print("I hope your day gets better")
+
+#Python Naming Convention Practice
+pi_value=3.14
+user_age=25
+user_location="New_york"
+MAXLIMIT=1000
+
+#Python Data Types and type() function
+variable_a="Hello World"
+variable_b=23
+variable_c=3.14
+variable_d=True
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+# Arithmetic Operations in Daily Life
+
+#Task1
+Eggs=3.99
+Yogurt=4.50
+one_pound_steak=5.50
+grocerie_total=(Eggs+Yogurt+one_pound_steak)
+print(grocerie_total)
+
+#Task2
+savings=10000
+interest_rate=.07
+total=(savings*interest_rate+savings)
+print(total)
+
+#Understand Assignments an Comparison
+a=56
+b=56
+print(a==b)
+#normally
+5+7*6
+42+5 
+#using parentheses
+5+(6*7)
+#mix and match
+4+7 > 10 or 22/2 > 9
+if 4+7 >10:
+     print("This is true")
+else:
+     print("Nah your wrong")
